292efacda8ef3a56228df08bf078bf66fe21a9f4
Chris <therstyle@gmail.com>
2020-04-27 11:52:12 -0400

    Added featured image component

--- src/components/FeaturedImage.vue
+++ src/components/FeaturedImage.vue
@@ -1,12 +1,12 @@
 <template>
   <figure class="featured-image">
-
+    <img :src="image[0].media_details.sizes.full.source_url" :alt="image[0].title.rendered">
   </figure>
 </template>
 
 <script>
 export default {
-  name:"Featured-Image",
+  name:"FeaturedImage",
   props: {
     image: Array
   }
