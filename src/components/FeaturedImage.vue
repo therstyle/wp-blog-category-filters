7633ea50590ae50f408b9f2087dd1108c012274e
Chris <therstyle@gmail.com>
2020-04-28 12:28:26 -0400

    Added function to generate srcset

--- src/components/FeaturedImage.vue
+++ src/components/FeaturedImage.vue
@@ -1,14 +1,31 @@
 <template>
   <figure class="featured-image">
-    <img :src="image[0].media_details.sizes.full.source_url" :alt="image[0].title.rendered">
+    <img :src="image[0].media_details.sizes['post-thumbnail']" :srcset="srcset" :alt="image[0].title.rendered">
   </figure>
 </template>
 
 <script>
 export default {
   name:"FeaturedImage",
+  computed: {
+    srcset() {
+      const sizes = this.image[0].media_details.sizes;
+      const srcset = [];
+
+      Object.keys(sizes).forEach(size => {
+        srcset.push(`${sizes[size].source_url} ${sizes[size].width}w`);
+      });
+
+      srcset.reverse();
+
+      return srcset.join();
+    }
+  },
   props: {
     image: Array
+  },
+  mounted() {
+    
   }
 }
 </script>
