d14ab0670f70540a7330d93fe8d0ccbe34e41691
Chris <therstyle@gmail.com>
2020-04-28 16:49:46 -0400

    Updated post body structure

--- src/components/post/PostCard.vue
+++ src/components/post/PostCard.vue
@@ -8,21 +8,23 @@
       ></FeaturedImage>
     </a>
 
-    <div v-if="displayDate || displayAuthor" class="eight29-post-detail">
-      <time v-if="displayDate">{{post.formatted_date}}</time>
-      <span v-if="displayAuthor" class="author">{{ post._embedded.author[0].name }}</span>
-    </div>
-    <h4 class="eight29-post-title"><a :href="post.link" v-html="post.title.rendered"></a></h4>
-    <div v-if="displayCategories" class="eight29-post-categories">
-      <a 
-        v-for="category in post._embedded['wp:term'][0]" 
-        :key="category.id"
-        :data-attribute-selected="currentCategoryIds.includes(category.id)"
-        v-on:click.prevent="replaceCurrentSelection({ category: category.slug, id: category.id, selected: currentCategoryIds.includes(category.id) })"
-      >{{ category.name }}</a>
-    </div>
+    <div class="eight29-post-body">
+      <div v-if="displayDate || displayAuthor" class="eight29-post-detail">
+        <time v-if="displayDate">{{post.formatted_date}}</time>
+        <span v-if="displayAuthor" class="author">{{ post._embedded.author[0].name }}</span>
+      </div>
+      <h4 class="eight29-post-title"><a :href="post.link" v-html="post.title.rendered"></a></h4>
+      <div v-if="displayCategories" class="eight29-post-categories">
+        <a 
+          v-for="category in post._embedded['wp:term'][0]" 
+          :key="category.id"
+          :data-attribute-selected="currentCategoryIds.includes(category.id)"
+          v-on:click.prevent="replaceCurrentSelection({ category: category.slug, id: category.id, selected: currentCategoryIds.includes(category.id) })"
+        >{{ category.name }}</a>
+      </div>
 
-    <div v-if="post.excerpt" class="eight29-post-excerpt" v-html="theExcerpt"></div>
+      <div v-if="post.excerpt" class="eight29-post-excerpt" v-html="theExcerpt"></div>
+    </div>
   </article>
 </template>
 
